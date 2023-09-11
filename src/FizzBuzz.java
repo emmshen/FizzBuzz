80466098557b4d3084485056b0f7cd13f24c4d15
emmshen <emmy12821@gmail.com>
2023-09-11 16:15:15 -0400

    - checked off the tasks I finished (up until 3.3) in readme file, added a main.java that used a method in Fizzbuzz, changed the method into public in Fizzbuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0; //sees if i divisible by 3
         boolean divisibleBy5 = i % 5 == 0; //sees if i divisible by 5
