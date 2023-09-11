e0cebd9ed430aa5fd3ecd177d253b04f88dee357
emmshen <emmy12821@gmail.com>
2023-09-11 15:50:20 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,30 +5,39 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i = 1;
+        while(i < 100) { //i starts at one, increases by 1 until it reaches 100
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            i = doFizzBuzz(i);
         }
+    }
+
+    private static int doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0; //sees if i divisible by 3
+        boolean divisibleBy5 = i % 5 == 0; //sees if i divisible by 5
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) { //&& means and
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) { //elif branch
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) { //elif branch
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+
+        // increment by 1
+        i += 1;
+        return i;
     }
 }
